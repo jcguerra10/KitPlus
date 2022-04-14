085d988904b26f5d4b9e44ad6393ff5c0e4d72c8
jcguerra10 <juancamiloguerratabares@gmail.com>
2022-04-14 12:46:44 -0500

    LogInActivityEmail

--- app/src/main/java/kitplus/project/app/activities/LogInActivity.kt
+++ app/src/main/java/kitplus/project/app/activities/LogInActivity.kt
@@ -1,12 +1,40 @@
 package kitplus.project.app.activities
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MotionEvent
+import android.view.inputmethod.InputMethodManager
 import kitplus.project.app.R
+import kitplus.project.app.databinding.ActivityLoginBinding
+import kitplus.project.app.databinding.ActivityMainBinding
 
 class LogInActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityLoginBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
+        binding = ActivityLoginBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+        binding.logInBtn.setOnClickListener {
+
+        }
+
+        binding.newAccountBtn.setOnClickListener {
+            
+        }
+
+    }
+
+    //touch and hide the keyboard
+    override fun dispatchTouchEvent(ev: MotionEvent?): Boolean {
+        if (currentFocus != null) {
+            val imm = this!!.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+            imm.hideSoftInputFromWindow(this!!.currentFocus!!.windowToken, 0)
+        }
+        return super.dispatchTouchEvent(ev)
     }
 }
--- app/src/main/java/kitplus/project/app/activities/MainActivity.kt
+++ app/src/main/java/kitplus/project/app/activities/MainActivity.kt
@@ -10,14 +10,13 @@ class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
 
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         val view = binding.root
         setContentView(view)
 
-        binding.logInBtn.setOnClickListener {
+        binding.emailBtn.setOnClickListener {
             val intent = Intent(this, LogInActivity::class.java)
             startActivity(intent)
         }
