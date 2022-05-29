06a37d0331a5b24365df6e525011bcf4e6f42bb9
jcguerra10 <juancamiloguerratabares@gmail.com>
2022-05-29 17:17:47 -0500

    Connection With User

--- app/src/main/java/kitplus/project/app/activities/ControllerActivity.kt
+++ app/src/main/java/kitplus/project/app/activities/ControllerActivity.kt
@@ -4,25 +4,43 @@ import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
+import com.google.gson.Gson
 import kitplus.project.app.R
 import kitplus.project.app.databinding.ActivityControllerBinding
+import kitplus.project.app.model.Profile
+import kitplus.project.app.model.User
 
 class ControllerActivity : AppCompatActivity() {
 
+    private lateinit var user: User
     private lateinit var binding: ActivityControllerBinding
 
     private lateinit var overviewFragment : OverviewFragment
     private lateinit var productivityFragment : ProductivityFragment
+
+    private val db = Firebase.firestore
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityControllerBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        val bundle = intent.extras
+        val usr = bundle?.getString("profile")
+        if (usr != null) {
+            setUpUser(usr)
+        } else {
+            val json = bundle?.getString("user")
+            user = Gson().fromJson(json, User::class.java)
+        }
+
         overviewFragment = OverviewFragment.newInstance()
         productivityFragment = ProductivityFragment.newInstance()
 
         showFragment(overviewFragment)
+
 
         binding.navigator.setOnItemSelectedListener { menuItem ->
             if (menuItem.itemId == R.id.overview){
@@ -35,7 +53,19 @@ class ControllerActivity : AppCompatActivity() {
 
         binding.profileImage.setOnClickListener {
             val intent = Intent(this, ProfileActivity::class.java)
+            val jsonString = Gson().toJson(user)
+            intent.putExtra("user", jsonString)
             startActivity(intent)
+        }
+    }
+
+    private fun setUpUser(userid: String?) {
+        val query = db.collection( "users"). whereEqualTo( "userid", userid)
+        query.get().addOnCompleteListener {
+            it.result!!.forEach {it ->
+                user = it.toObject(User::class.java)
+                binding.userName.text = user.name
+            }
         }
     }
 
--- app/src/main/java/kitplus/project/app/activities/ProfileActivity.kt
+++ app/src/main/java/kitplus/project/app/activities/ProfileActivity.kt
@@ -2,11 +2,31 @@ package kitplus.project.app.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Editable
+import androidx.core.text.set
+import com.google.gson.Gson
 import kitplus.project.app.R
+import kitplus.project.app.databinding.ActivityProfileBinding
+import kitplus.project.app.model.User
 
 class ProfileActivity : AppCompatActivity() {
+
+    private lateinit var user: User
+    private lateinit var binding: ActivityProfileBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_profile)
+        binding = ActivityProfileBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val bundle = intent.extras
+        val json = bundle?.getString("user")
+        user = Gson().fromJson(json, User::class.java)
+
+        binding.nameView.text = user.name
+        binding.birthdayView.text = user.birthday
+        binding.weightEdit.setText(user.weightUser)
+        binding.heightEditText.setText(user.heightUser)
+
     }
 }
