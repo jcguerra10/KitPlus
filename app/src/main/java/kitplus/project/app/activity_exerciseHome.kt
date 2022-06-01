43fa4701ce23ed9dd4743e3178a2a049045d8b84
Dtorres1ro <46894535+Datorresp@users.noreply.github.com>
2022-06-01 11:58:44 -0500

    Exercise Activity

---
+++ app/src/main/java/kitplus/project/app/activity_exerciseHome.kt
@@ -0,0 +1,11 @@
+package kitplus.project.app
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class activity_exerciseHome : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_exercise_home)
+    }
+}
