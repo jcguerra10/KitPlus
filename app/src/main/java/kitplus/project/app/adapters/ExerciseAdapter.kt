f74fce7f988ef80f4f647509c30c7527f733d3f3
Dtorres1ro <46894535+Datorresp@users.noreply.github.com>
2022-06-02 10:43:21 -0500

    Exercise Item, and adapter

---
+++ app/src/main/java/kitplus/project/app/adapters/ExerciseAdapter.kt
@@ -0,0 +1,25 @@
+package kitplus.project.app.adapters
+
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import kitplus.project.app.model.ExerciseRegistry
+
+class ExerciseAdapter: RecyclerView.Adapter<ExerciseView>() {
+
+    private var exercises = ArrayList<ExerciseRegistry>()
+
+    
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ExerciseView {
+        TODO("Not yet implemented")
+    }
+
+    override fun onBindViewHolder(holder: ExerciseView, position: Int) {
+        TODO("Not yet implemented")
+    }
+
+    override fun getItemCount(): Int {
+        TODO("Not yet implemented")
+    }
+
+
+}
